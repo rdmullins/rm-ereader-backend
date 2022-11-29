594b816b0541ae7cc72da613fcdeab89f762c81a
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-29 21:27:52 +0000

    Filtered partial search working for title

--- e_reader/ereader/urls.py
+++ e_reader/ereader/urls.py
@@ -5,8 +5,11 @@ from rest_framework import routers
 
 router = routers.SimpleRouter()
 router.register(r'books_api', views.BookViewSet)
+router.register(r"author_books", views.Author_BookViewSet)
+#router.register(r"booksearch", views.BookSearch)
 
 urlpatterns = [
     path('', views.books),
     path('', include(router.urls)),
+    path('booksearch/', views.BookSearch.as_view(), name="BookSearch"),
 ]
