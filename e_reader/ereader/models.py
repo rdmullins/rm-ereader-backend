8a9cd8219be516fc305e09e2893569dc4428d02d
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-29 15:09:27 +0000

    Defined return values for tables

--- e_reader/ereader/models.py
+++ e_reader/ereader/models.py
@@ -15,14 +15,26 @@ class Author(models.Model):
     dob = models.DateField(blank=True)
     dod = models.DateField(blank=True)
 
+    def __str__(self):
+        return '%s, %s (%s - %s)' % (self.last_name, self.first_name, self.dob, self.dod)
+
 class Author_Role(models.Model):
     role = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.role
 
 class Subject(models.Model):
     subject = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.subject
+
 class Gutenberg_Type(models.Model):
     type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.type
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -32,8 +44,14 @@ class Book(models.Model):
     description = models.TextField(default="No Description Available")
     gut_type = models.ForeignKey(Gutenberg_Type, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return '%s (%s, %s)\n\tGutenberg ID: %s\n\tLibrivox ID: %s\nDescription:\n\%s' %(self.title, self.gut_issued, self.gut_type, self.gut_id, self.lib_id, self.description)
+
 class Book_Status(models.Model):
     status = models.CharField(max_length=255)
+
+    def __str__(self):
+        self.status
 
 class User_Book(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
@@ -44,23 +62,41 @@ class User_Book(models.Model):
     updated = models.DateTimeField(auto_now=True)
     is_visible = models.BooleanField(default=True)
 
+    def __str__(self):
+        return 'User: %s\nBook: %s' % (self.user.username, self.book.title)
+
 class Collection(models.Model):
     name = models.CharField(max_length=255)
     is_public = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 class User_Collection(models.Model):
     collection = models.ManyToManyField(Collection)
     user = models.ManyToManyField(CustomUser)
+
+    def __str__(self):
+        return self.collection.name
 
 class Author_Book(models.Model):
     author = models.ManyToManyField(Author)
     book = models.ManyToManyField(Book)
     author_role = models.ForeignKey(Author_Role, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return ('%s, %s (%s)') % (self.author, self.book.title, self.author_role.role)
+
 class Subject_Book(models.Model):
     subject = models.ManyToManyField(Subject)
     book = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return ('%s, %s') % (self.subject.subject, self.book.title)
+
 class Collection_Book(models.Model):
     collection = models.ManyToManyField(Collection)
     book = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return ('%s, %s') % (self.collection.name, self.book.title)
